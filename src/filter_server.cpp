c580aea67d6955d1c1251b8ab04eca77578ee3b5
Timothy Chung <timchunght@gmail.com>
2016-10-27 15:05:05 -0700

    Configured pc_filter to work with fetch22 using yaml config

--- src/filter_server.cpp
+++ src/filter_server.cpp
@@ -21,6 +21,7 @@ float z_clip_max_;
 
 void filterCallback(const sensor_msgs::PointCloud2ConstPtr& sensor_message_pc)
 {
+  ROS_INFO("Filtering PointCloud Callback");
   pcl::PCLPointCloud2 original_pc2;
   pcl_conversions::toPCL(*sensor_message_pc, original_pc2);
 
@@ -29,8 +30,8 @@ void filterCallback(const sensor_msgs::PointCloud2ConstPtr& sensor_message_pc)
 
   tf::StampedTransform transform;
   ros::Time now = ros::Time::now();
-  tf_listener->waitForTransform ("/world", "/camera_rgb_optical_frame", now, ros::Duration(4.0));
-  tf_listener->lookupTransform ("/world", "/camera_rgb_optical_frame", now, transform);
+  tf_listener->waitForTransform ("/world", "/head_camera_rgb_optical_frame", now, ros::Duration(2.0));
+  tf_listener->lookupTransform ("/world", "/head_camera_rgb_optical_frame", now, transform);
 
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZRGB>());
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered_x(new pcl::PointCloud<pcl::PointXYZRGB>());
@@ -38,7 +39,7 @@ void filterCallback(const sensor_msgs::PointCloud2ConstPtr& sensor_message_pc)
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered_xyz(new pcl::PointCloud<pcl::PointXYZRGB>());
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_transformed_back(new pcl::PointCloud<pcl::PointXYZRGB>());
 
-  original_pc->header.frame_id = "/camera_rgb_optical_frame";
+  original_pc->header.frame_id = "/head_camera_rgb_optical_frame";
   pcl_ros::transformPointCloud("/world",
           *original_pc,
           *cloud_transformed,
@@ -46,60 +47,71 @@ void filterCallback(const sensor_msgs::PointCloud2ConstPtr& sensor_message_pc)
 
   pcl::PassThrough<pcl::PointXYZRGB > pass;
   pass.setInputCloud(cloud_transformed);
+
+  ROS_INFO("filtercloudsize before x: ");
+  ROS_INFO_STREAM(cloud_transformed->size());
+
   pass.setFilterFieldName ("x");
   pass.setFilterLimits (x_clip_min_, x_clip_max_);
   pass.filter(*cloud_filtered_x);
 
+
+  ROS_INFO("filtercloudsize before y: ");
+  ROS_INFO_STREAM(cloud_filtered_x->size());
+  
   pass.setInputCloud(cloud_filtered_x);
   pass.setFilterFieldName ("y");
   pass.setFilterLimits (y_clip_min_, y_clip_max_);
   pass.filter(*cloud_filtered_xy);
 
   pass.setInputCloud(cloud_filtered_xy);
+  
+
+  ROS_INFO("filtercloudsizei before z: ");
+  ROS_INFO_STREAM(cloud_filtered_xy->size());
+  
   pass.setFilterFieldName ("z");
   pass.setFilterLimits (z_clip_min_, z_clip_max_);
   pass.filter(*cloud_filtered_xyz);
 
   cloud_filtered_xyz->header.frame_id = "/world";
-  pcl_ros::transformPointCloud("/camera_rgb_optical_frame",
+  pcl_ros::transformPointCloud("/head_camera_rgb_optical_frame",
 		      *cloud_filtered_xyz,
 		      *cloud_transformed_back,
 		      *tf_listener);
 
-  cloud_transformed_back->header.frame_id = "/camera_rgb_optical_frame";
+  cloud_transformed_back->header.frame_id = "/head_camera_rgb_optical_frame";
 
   pcl::PCLPointCloud2 cloud_transformed_back_pc2;
   pcl::toPCLPointCloud2(*cloud_transformed_back, cloud_transformed_back_pc2);
 
   sensor_msgs::PointCloud2 cloud_transformed_back_msg;
   pcl_conversions::fromPCL(cloud_transformed_back_pc2, cloud_transformed_back_msg);
-
+  ROS_INFO("filtercloudsize: ");
+  ROS_INFO_STREAM(cloud_transformed_back->size());
   filtered_pc_pub.publish(cloud_transformed_back_msg);
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "filteredpcserver");
-  n = new ros::NodeHandle();
+  ros::NodeHandle n;
   tf::TransformListener tfl;
   tf_listener = &tfl;
 
-  /*n->getParam("x_clip_min", x_clip_min_);
-  n->getParam("x_clip_max", x_clip_max_);
-  n->getParam("y_clip_min", y_clip_min_);
-  n->getParam("y_clip_max", y_clip_max_);
-  n->getParam("z_clip_min", z_clip_min_);
-  n->getParam("z_clip_max", z_clip_max_);*/
+  n.getParam("/xpassthrough/filter_limit_min", x_clip_min_);
+  n.getParam("/xpassthrough/filter_limit_max", x_clip_max_); 
+  n.getParam("/ypassthrough/filter_limit_min", y_clip_min_);
+  n.getParam("/ypassthrough/filter_limit_max", y_clip_max_); 
+  n.getParam("/zpassthrough/filter_limit_min", z_clip_min_);
+  n.getParam("/zpassthrough/filter_limit_max", z_clip_max_);
 
-  x_clip_min_ = -0.2;
-  x_clip_max_ = 0.23;
-  y_clip_min_ = -0.65;
-  y_clip_max_ = 0.1;
-  z_clip_min_ = 0.0;
-  z_clip_max_ = 0.4;
+  ros::Time now = ros::Time::now();
+  ROS_INFO("WAITING for Checkerboard detection to find world frame");
+  tf_listener->waitForTransform ("/world", "/head_camera_rgb_optical_frame", now, ros::Duration(10.0));
 
-  ros::Subscriber original_pc_sub = n->subscribe("/camera/depth_registered/points", 1, filterCallback);
-  filtered_pc_pub = n->advertise<sensor_msgs::PointCloud2>("filtered_pc", 1);
+  ros::Subscriber original_pc_sub = n.subscribe("/head_camera/depth_registered/points", 1, filterCallback);
+  filtered_pc_pub = n.advertise<sensor_msgs::PointCloud2>("filtered_pc", 1);
 
   ros::spin();
   return 0;
